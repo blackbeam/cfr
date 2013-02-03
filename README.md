7e9fa209f112ab4a09262615a6afe053c93943a6
blackbeam <set@localhost.localdomain>
2013-02-03 20:35:10 +0400

    Plugin created

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
 cfr
 ===
 
-jQuery chunked file read plugin
+jQuery plugin for chunked file reading.
+
+### Basic setup
+
+```javascript
+$('input[type=file]').cfr();
+```
+
+### Options
+
+* `chunkSize` - chunk size in bytes.
+* `readFunc` - function to call on [FileReader](https://developer.mozilla.org/en-US/docs/DOM/FileReader) (`readAsDataURL` by default ).
+* `filter` - function to call on chunk before pass it to `onChunk` callback (`function (c) { return c.substr(c.indexOf(',') + 1); }` by default).
+
+If you override a `readFunc` you also need override a default `filter` or pass it a `null`.
+
+### Callbacks
+
+* `onStart(file)` - will be called before reading first chunk.
+  * `file` - [File](https://developer.mozilla.org/en-US/docs/DOM/File) object.
+* `onChunk(chunk, size, chunkNum, chunks, cb)` - will be called on each chunk.
+  * `chunk` - output of `filter` function if present. Instead a result of FileReaders `onloadend` event.
+  * `size` - count of bytes read from `file`.
+  * `chunkNum` - number of chunk.
+  * `chunks` - count of chunks in file.
+  * `cb` - function you must call to continue reading. You also can call `cb(true)` to cancel reading and trigger `onCancel` callback.
+* `onCancel(file)` - will be called after `cb(true)` or `.cfr('cancel')`.
+  * `file` - [File](https://developer.mozilla.org/en-US/docs/DOM/File) object.
+* `onEnd(file)` - will be called after last chunk read.
+  * `file` - [File](https://developer.mozilla.org/en-US/docs/DOM/File) object.
+
+### Methods
+
+* `.cfr('cancel')` - cancel reading process
+* `.cfr('remove')` - removing *cfr* from element (does not abort read process)
+
+### Example
+
+See test.html for complete example.
---
+++ jquery.cfr.js
@@ -0,0 +1,140 @@
+/*
+ * Read file chunk by chunk with HTML5 FileReader API
+ */
+(function ($) {
+    "use strict";
+    var plugin_name = 'cfr';
+    var window = this;
+
+    var isApiSupported = !!(window.File && window.FileReader && window.FileReader.prototype.readAsDataURL && window.Blob);
+    var sliceName = 'slice';
+    if (isApiSupported) {
+        if (window.Blob.prototype.webkitSlice) {
+            sliceName = 'webkitSlice';
+        }
+        else if (window.Blob.prototype.mozSlice) {
+            sliceName = 'mozSlice';
+        }
+    }
+    var filter = function (c) {
+        return c.substr(c.indexOf(',') + 1);
+    };
+
+    var methods = {
+        init: function (options) {
+            var $this = this;
+            var opts = $.extend({
+                chunkSize: 2048 * 1024,
+                filter: filter,
+                readFunc: 'readAsDataURL'
+            }, options);
+            var lastFileName = null;
+            var cancelled = false;
+            var data = $this.data(plugin_name);
+
+            if (! data) {
+                $this.data(plugin_name, {
+                    opts: opts,
+                    lastFileName: lastFileName,
+                    cancelled: cancelled,
+                    changeListener: null
+                });
+                data = $this.data(plugin_name);
+            } else {
+                if (data.changeListener) {
+                    $this.unbind('change', data.changeListener);
+                }
+                data.opts = opts;
+                data.lastFileName = lastFileName;
+                data.cancelled = cancelled;
+                data.changeListener = null;
+            }
+
+            if (isApiSupported && $this.prop('tagName') === 'INPUT' && $this.attr('type') === 'file') {
+                $this.change(data.changeListener = function (ev) {
+                    var file = ev.currentTarget && ev.currentTarget.files[0];
+                    if (!file) { return false; }
+
+                    var fileName = file.name || file.fileName || null;
+                    if (data.lastFileName === fileName) { return false; }
+                    data.lastFileName = fileName;
+
+                    var fileSize = file.size || file.fileSize || null;
+                    if (!fileSize) { return false; }
+
+                    var chunk = 1;
+                    var chunkCount = Math.ceil(fileSize / data.opts.chunkSize);
+
+                    var read = function (chunkNum) {
+                        var offset = data.opts.chunkSize * (chunkNum - 1);
+                        if (offset >= fileSize) {
+                            if (data.opts.onEnd && 'function' === typeof data.opts.onEnd) {
+                                data.opts.onEnd.call($this, file);
+                            }
+                        } else {
+                            var blob = file[sliceName](offset, offset + data.opts.chunkSize);
+                            var reader = new window.FileReader();
+                            reader.onloadend = function (ev) {
+                                if (ev.target.readyState === window.FileReader.DONE) {
+                                    var chunk = ev.target.result;
+                                    chunk = (data.opts.filter && 'function' === typeof data.opts.filter) ? data.opts.filter(chunk) : chunk;
+                                    var chunkSize = blob.size || blob.fileSize;
+                                    if (! data.cancelled) {
+                                        data.opts.onChunk.call($this, chunk, chunkSize, chunkNum, chunkCount, function (cancel) {
+                                            if (! cancel) {
+                                                read(++chunkNum);
+                                            } else {
+                                                if (data.opts.onCancel && 'function' === typeof data.opts.onCancel) {
+                                                    data.opts.onCancel.call($this, file);
+                                                }
+                                            }
+                                        });
+                                    } else {
+                                        if (data.opts.onCancel && 'function' === typeof data.opts.onCancel) {
+                                            data.cancelled = false;
+                                            data.opts.onCancel.call($this, file);
+                                        }
+                                    }
+                                }
+                            };
+                            reader[data.opts.readFunc](blob);
+                        }
+                    };
+                    if (data.opts.onStart && 'function' === typeof data.opts.onStart) {
+                        data.opts.onStart.call($this, file);
+                    }
+                    read(chunk);
+                });
+            }
+
+            return $this;
+        },
+        cancel: function () {
+            var $this = this;
+            var data = $this.data(plugin_name);
+            if (data && data.cancelled === false) {
+                data.cancelled = true;
+            }
+            return $this;
+        },
+        remove: function () {
+            var $this = this;
+            var data = $this.data(plugin_name);
+            if (data) {
+                $this.unbind('change', data.changeListener);
+                $this.removeData(plugin_name);
+            }
+            return $this;
+        }
+    };
+
+    $.fn[plugin_name] = function (method) {
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if ('object' === typeof method || ! method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + method + ' does not exist on jQuery.' + plugin_name);
+        }
+    };
+}).call(window, window.jQuery);
